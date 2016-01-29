245abcd1a1a1042286df9805d9d7fe8eb8ddf55e
Eduardo_Vazquez <eduardo.vazquezn@gmail.com>
2016-01-29 23:30:17 +0100

    Añadidos scritps para trampas y mecanicas

---
+++ RitualJam/Assets/Boulder.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boulder : MonoBehaviour {
+
+    public enum Directions { North, South, West, East}
+    public Directions direction;
+    public float speed;
+    private Vector3 movement;
+    private CharacterController controller;
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        switch (direction)
+        {
+            case Directions.North:
+                movement = new Vector3(0, 1,0);
+                break;
+            case Directions.South:
+                movement = new Vector3(0, -1,0);
+                break;
+            case Directions.East:
+                movement = new Vector3(1, 0,0);
+                break;
+            case Directions.West:
+                movement = new Vector3(-1, 0,0);
+                break;
+            default:
+                break;
+        }
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        //rb2d.AddForce(movement * speed);
+
+        controller.Move(movement * speed);
+    }
+
+
+}
---
+++ RitualJam/Assets/GroundHole.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundHole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Player Died");
+            //col.getCompontent<PlayerDeath>().kill();
+
+        }
+    }
+}
