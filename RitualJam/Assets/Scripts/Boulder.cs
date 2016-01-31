2abd980f4b4e14c147383fdd28187b53d060a514
JuanCarlosGdM <juancarlosgdm@gmail.com>
2016-01-31 01:39:29 +0100

    - Contador de muertes completo

--- RitualJam/Assets/Scripts/Boulder.cs
+++ RitualJam/Assets/Scripts/Boulder.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 
 public class Boulder : MonoBehaviour {
-
     public enum Directions { North, South, West, East}
     public Directions direction;
     public float speed;
+
     private Vector3 movement;
     private CharacterController controller;
+
     // Use this for initialization
     void Start () {
-        controller = GetComponent<CharacterController>();
+//        controller = GetComponent<CharacterController>();
         switch (direction)
         {
             case Directions.North:
@@ -28,23 +29,28 @@ public class Boulder : MonoBehaviour {
             default:
                 break;
         }
-
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.Translate(movement*Time.deltaTime);
 	}
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         //rb2d.AddForce(movement * speed);
 
-        controller.Move(movement * speed);
+//        controller.Move(movement * speed);
     }
 
-
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag.Equals("Player"))	{
+			Debug.Log("Player Died");
+			col.gameObject.GetComponent<GridMove>().setIsDead(true);
+			col.gameObject.GetComponent<SpriteRenderer>().enabled=false;
+			col.gameObject.GetComponent<PlayerDeath>().Kill(1, GameStats.deathTypes.Boulder);
+			//Instantiate(spikes,this.transform.position,this.transform.rotation);
+		}
+	}
 }
